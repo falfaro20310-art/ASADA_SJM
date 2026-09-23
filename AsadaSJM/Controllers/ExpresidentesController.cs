ff32abe680c71f394ddb419fbfa2d4c52177852c
jhoel29 <jhoelq29@gmail.com>
2026-09-23 15:18:59 -0600

    Implementar MPI-05, MPI-06 y MPI-07

---
+++ AsadaSJM/Controllers/ExpresidentesController.cs
@@ -0,0 +1,140 @@
+﻿using AsadaSJM.Data;
+using AsadaSJM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsadaSJM.Controllers;
+
+public class ExpresidentesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ExpresidentesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Expresidentes
+    public async Task<IActionResult> Index()
+    {
+        var presidentes = await _context.Expresidentes
+            .AsNoTracking()
+            .OrderByDescending(e => e.Estado)
+            .ThenByDescending(e => e.IdExpresidente)
+            .ToListAsync();
+
+        return View(presidentes);
+    }
+
+    // GET: Expresidentes/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Expresidentes/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(
+        [Bind("Nombre,Trayectoria,Proyectos,Imagen,Estado")]
+        Expresidente expresidente)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(expresidente);
+        }
+
+        if (expresidente.Estado)
+        {
+            var presidentesActuales = await _context.Expresidentes
+                .Where(e => e.Estado)
+                .ToListAsync();
+
+            foreach (var presidente in presidentesActuales)
+            {
+                presidente.Estado = false;
+            }
+        }
+
+        _context.Add(expresidente);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Expresidentes/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var expresidente = await _context.Expresidentes
+            .FindAsync(id);
+
+        if (expresidente is null)
+        {
+            return NotFound();
+        }
+
+        return View(expresidente);
+    }
+
+    // POST: Expresidentes/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(
+        int id,
+        [Bind("IdExpresidente,Nombre,Trayectoria,Proyectos,Imagen,Estado")]
+        Expresidente expresidente)
+    {
+        if (id != expresidente.IdExpresidente)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(expresidente);
+        }
+
+        if (expresidente.Estado)
+        {
+            var presidentesActuales = await _context.Expresidentes
+                .Where(e =>
+                    e.Estado &&
+                    e.IdExpresidente != expresidente.IdExpresidente)
+                .ToListAsync();
+
+            foreach (var presidente in presidentesActuales)
+            {
+                presidente.Estado = false;
+            }
+        }
+
+        _context.Update(expresidente);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // POST: Expresidentes/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var expresidente = await _context.Expresidentes
+            .FindAsync(id);
+
+        if (expresidente is null)
+        {
+            return NotFound();
+        }
+
+        _context.Expresidentes.Remove(expresidente);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ AsadaSJM/Models/Expresidente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsadaSJM.Models;
+
+public class Expresidente
+{
+    [Key]
+    public int IdExpresidente { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Nombre { get; set; } = string.Empty;
+
+    public string? Trayectoria { get; set; }
+
+    public string? Proyectos { get; set; }
+
+    [StringLength(500)]
+    public string? Imagen { get; set; }
+
+    [Required]
+    public bool Estado { get; set; }
+}
